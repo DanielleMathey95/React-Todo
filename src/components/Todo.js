655a25e38ffbfffe93ef4a11acfd20f843121789
DanielleMathey95 <danielle.mathey@yahoo.com>
2020-07-08 19:15:59 -0700

    Just have to finish the render portion of App and then move on to styling

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,6 +4,10 @@ const Todo = props => {
   console.log("Todo JS: ", props);
 
   return (
-    <div></div>
-  )
-}
+    <div className={`Todo${!props.task.completed ? "completed" : ""}`} onClick={() => props.toggleComplete(props.task.id)}>
+      <p>{props.task.task}</p>
+    </div>
+  );
+};
+
+export default Todo;
